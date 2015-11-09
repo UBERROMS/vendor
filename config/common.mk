b584b60b42b086beb0739b6fefb003c7a1c54b31
despairfactor <matthewdalex92@gmail.com>
2015-11-09 12:24:33 -0500

    config: add filter to camera effect in common.mk

--- config/common.mk
+++ config/common.mk
@@ -60,10 +60,11 @@ PRODUCT_COPY_FILES += \
     vendor/uber/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 
 # Camera Effects
+ifneq ($(filter uber_flounder uber_hammerhead uber_shamu,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES +=  \
     vendor/uber/prebuilt/vendor/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
     vendor/uber/prebuilt/vendor/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
-
+endif
 # Backuptool support
 PRODUCT_COPY_FILES += \
     vendor/uber/prebuilt/addon.d/50-uber.sh:system/addon.d/50-uber.sh \
