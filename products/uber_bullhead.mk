0bf77b24b71149c267b864300b29de8b5e0a9c7a
Chet Kener <Cl3Kener@gmail.com>
2015-12-07 21:39:13 -0500

    Update fingerprints to 6.0.1 MMB29M
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- products/uber_bullhead.mk
+++ products/uber_bullhead.mk
@@ -13,8 +13,8 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0/MDB08M/2353240:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0 MDB08M 2353240 release-keys"
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MMB29M/2431559:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MMB29M 2431559 release-keys"
 
 # Boot animation
 PRODUCT_COPY_FILES += \
