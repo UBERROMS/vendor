721cbe29da96b4e3ad0a32208617cdabe726d438
DespairFactor <matthewdalex92@gmail.com>
2016-01-04 17:55:46 -0500

    products: update shamu fingerprint to MMB29S

--- products/uber_shamu.mk
+++ products/uber_shamu.mk
@@ -23,7 +23,7 @@ PRODUCT_NAME := uber_shamu
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="shamu" BUILD_FINGERPRINT="google/shamu/shamu:6.0.1/MMB29M/2431559:user/release-keys" PRIVATE_BUILD_DESC="shamu-user 6.0.1 MMB29M 2431559 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="shamu" BUILD_FINGERPRINT="google/shamu/shamu:6.0.1/MMB29S/2489379:user/release-keys" PRIVATE_BUILD_DESC="shamu-user 6.0.1 MMB29S 2489379 release-keys"
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
