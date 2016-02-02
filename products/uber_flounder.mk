f07634bbe6e8b4d00078f833877c4512a61f9c4a
DespairFactor <matthewdalex92@gmail.com>
2016-02-02 14:43:48 -0500

    products: flounder: update fingerprint to MMB29R

--- products/uber_flounder.mk
+++ products/uber_flounder.mk
@@ -23,8 +23,11 @@ PRODUCT_NAME := uber_flounder
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 9
 PRODUCT_MANUFACTURER := htc
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="flounder" BUILD_FINGERPRINT="google/flounder/flounder:6.0.1/MMB29S/2489379:user/release-keys" PRIVATE_BUILD_DESC="flounder-user 6.0.1 MMB29S 2489379 release-keys"
-
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=flounder \
+    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29R/2482564:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29R 2482564 release-keys"
+    
 # Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/uber/prebuilt/bootanimation/bootanimation_1080.zip:system/media/bootanimation.zip
