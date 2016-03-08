988fa1fcb2fed94dc730b25a2cdde43bdde0fc78
DespairFactor <matthewdalex92@gmail.com>
2016-03-08 05:10:57 -0500

    Update angler, bullhead, flo, flounder, mako and shamu to MMB29V

--- products/uber_flounder.mk
+++ products/uber_flounder.mk
@@ -23,11 +23,8 @@ PRODUCT_NAME := uber_flounder
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 9
 PRODUCT_MANUFACTURER := htc
-PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29R/2482564:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29R 2482564 release-keys"
-    
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="flounder" BUILD_FINGERPRINT="google/flounder/flounder:6.0.1/MMB29V/2554798:user/release-keys" PRIVATE_BUILD_DESC="flounder-user 6.0.1 MMB29V 554798 release-keys"
+
 # Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/uber/prebuilt/bootanimation/bootanimation_1080.zip:system/media/bootanimation.zip
