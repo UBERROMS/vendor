c4c22e18a9a7d1966c5cc38a495a0cc1e20571e5
Chet Kener <Cl3Kener@gmail.com>
2016-08-03 08:00:35 -0400

    Update Hammerhead Fingerprint
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- products/uber_hammerhead.mk
+++ products/uber_hammerhead.mk
@@ -23,7 +23,7 @@ PRODUCT_NAME := uber_hammerhead
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="hammerhead" BUILD_FINGERPRINT="google/hammerhead/hammerhead:6.0.1/MOB30P/2960889:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30P 2960889 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="hammerhead" BUILD_FINGERPRINT="google/hammerhead/hammerhead:6.0.1/MOB30Y/3067468:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30Y 3067468 release-keys"
 # Kernel inline build
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 TARGET_KERNEL_CONFIG := uber_hammerhead_defconfig
