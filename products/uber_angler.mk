4128e351d04d86374a0d76b6686851e0d0ce63d2
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2016-09-02 06:07:20 +0200

    Fix angler build fingerprint

--- products/uber_angler.mk
+++ products/uber_angler.mk
@@ -6,15 +6,15 @@ $(call inherit-product, vendor/uber/config/common.mk)
 
 # Setup device specific product configuration.
 PRODUCT_NAME := uber_angler
-PRODUCT_BRAND := google
+PRODUCT_BRAND := Google
 PRODUCT_DEVICE := angler
 PRODUCT_MODEL := Nexus 6P
 PRODUCT_MANUFACTURER := Huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:7.0.0/NRD90M/3085278:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 7.0.0 NRD90M 3085278 release-keys" 
+    BUILD_FINGERPRINT=google/angler/angler:7.0/NRD90M/3085278:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.0 NRD90M 3085278 release-keys"
 
 # Boot animation
 PRODUCT_COPY_FILES += \
