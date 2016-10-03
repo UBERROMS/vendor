c1d13b62b44086eaad99168c29e7561cc973ab19
DespairFactor <matthewalex@linux.com>
2016-10-03 14:50:34 -0400

    products: update Bullhead fingerprint to NBD90W

--- products/uber_bullhead.mk
+++ products/uber_bullhead.mk
@@ -13,8 +13,8 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:7.0/NRD90S/3142244:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 7.0 NRD90S 3142244 release-keys" 
+    BUILD_FINGERPRINT=google/bullhead/bullhead:7.0/NBD90W/3239497:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 7.0 NBD90W 3239497 release-keys" 
 
 # Boot animation
 PRODUCT_COPY_FILES += \
