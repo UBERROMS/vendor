50eb0f83973998a5c206cb8a91d1d09dcef37495
Matthew Alex <matthewalex@outlook.com>
2016-10-03 19:45:23 -0400

    products: update Shamu fingerprint to NBD90Z

--- products/uber_shamu.mk
+++ products/uber_shamu.mk
@@ -25,8 +25,8 @@ PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.0.0/NRD90M/3085278:user/release-keys \
-	PRIVATE_BUILD_DESC="shamu-user 7.0.0 NRD90M 3085278 release-keys" 
+    BUILD_FINGERPRINT=google/shamu/shamu:7.0.0/NBD90Z/3264873:user/release-keys \
+	PRIVATE_BUILD_DESC="shamu-user 7.0.0 NBD90Z 3264873 release-keys" 
 # Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/uber/prebuilt/bootanimation/bootanimation_1080.zip:system/media/bootanimation.zip
