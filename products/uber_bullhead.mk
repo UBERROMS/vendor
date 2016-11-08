9f024751b10a290d8ce790b7993dd1c13a729297
Matthew Alex <matthewalex@outlook.com>
2016-11-08 14:37:19 -0500

    products: update bullhead fingerprint to NRD91N

--- products/uber_bullhead.mk
+++ products/uber_bullhead.mk
@@ -13,9 +13,11 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:7.0/NBD90W/3239497:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 7.0 NBD90W 3239497 release-keys" 
+    BUILD_FINGERPRINT=google/bullhead/bullhead:7.0/NRD91N/3318108:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 7.0 NRD91N 3318108 release-keys" 
 
 # Boot animation
 PRODUCT_COPY_FILES += \
     vendor/uber/prebuilt/bootanimation/bootanimation_1080.zip:system/media/bootanimation.zip
+
+
