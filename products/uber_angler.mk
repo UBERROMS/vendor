40cb5e8b8c5f2ba406b476e3c84e86de4d996187
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2017-01-05 22:02:30 +0100

    Angler N4F26J

--- products/uber_angler.mk
+++ products/uber_angler.mk
@@ -6,15 +6,15 @@ $(call inherit-product, vendor/uber/config/common.mk)
 
 # Setup device specific product configuration.
 PRODUCT_NAME := uber_angler
-PRODUCT_BRAND := Google
+PRODUCT_BRAND := google
 PRODUCT_DEVICE := angler
 PRODUCT_MODEL := Nexus 6P
 PRODUCT_MANUFACTURER := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:7.1.1/NMF26F/3425388:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 7.1.1 NMF26F 3425388 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/N4F26J/3549317:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 N4F26J 3549317 release-keys"
 
 # Boot animation
 PRODUCT_COPY_FILES += \
